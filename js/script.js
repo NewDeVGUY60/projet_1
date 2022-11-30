bb1115aab0e3735878ffbe5646015baea7ec2a34
NewDeVGUY60 <[guynicolas60@gmail.com](mailto:guynicolas60@gmail.com)>
2022-11-30 12:24:59 +0100

    débug css and test js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const btn = document.querySelector('.btn');
 const menu = document.querySelector('.menu-contener');
-btn.addEventListener('click', () => {
-    menu.classList.toggle('active');
+btn.addEventListener('click', (e) => {
+
+    // menu.classList.toggle('active');
+    menu.style.right = "-200px";
 });
+
+
 // const btn2 = document.querySelector('.btn-contact');
 // const menu2 = document.querySelector('.contact');
 

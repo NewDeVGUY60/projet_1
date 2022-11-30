6ff847df70ba4c52728ebbb38cc9361da145636f
NewDeVGUY60 <[guynicolas60@gmail.com](mailto:guynicolas60@gmail.com)>
2022-11-30 10:07:43 +0100

    change html and css for responsive

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.btn');
+const menu = document.querySelector('.menu-contener');
+btn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+// const btn2 = document.querySelector('.btn-contact');
+// const menu2 = document.querySelector('.contact');
+
+// btn2.addEventListener('click', () => {
+//     menu2.classList.toggle('active');
+// });

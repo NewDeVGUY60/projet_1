a956ea2074f90abb331003e403277c8c741d64ea
NewDeVGUY60 <[guynicolas60@gmail.com](mailto:guynicolas60@gmail.com)>
2022-12-01 10:55:30 +0100

    add toggle for destop-menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
+// Au clique sur le menu smartphone afficher ou cacher le menu .
+
 const btn = document.querySelector('.btn');
-const active = document.querySelector('.active');
 const menu = document.querySelector('.menu-contener');
-btn.addEventListener('click', (e) => {
-    menu.classList.toggle('active');
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle("active");
 });
 
+// Au survol du boutton contact affichez les contacts et les laisser visible tant que l'on survol est fait 
 
-// const btn2 = document.querySelector('.btn-contact');
-// const menu2 = document.querySelector('.contact');
+const btnContact = document.querySelector('.btn-contact');
+const contact = document.querySelector('.contact2');
 
-// btn2.addEventListener('click', () => {
-//     menu2.classList.toggle('active');
-// });
+btnContact.addEventListener('click', () => {
+    contact.classList.toggle("visible");
+});
+// Au survol du boutton photo affichez les contacts et les laisser visible tant que l'on survol est fait
+const btnPhoto = document.querySelector('.btn-photo');
+const photo = document.querySelector('.picture2');
+
+btnPhoto.addEventListener('click', () => {
+    photo.classList.toggle("visible");
+});
+// Au survol du boutton video affichez les contacts et les laisser visible tant que l'on survol est fait
+
+const btnVideo = document.querySelector('.btn-video');
+const video = document.querySelector('.video2');
+
+btnVideo.addEventListener('click', () => {
+    video.classList.toggle("visible");
+});
+

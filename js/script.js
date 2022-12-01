79fdcf5b2ea21092cd5b8ac285ad617d24f57091
NewDeVGUY60 <[guynicolas60@gmail.com](mailto:guynicolas60@gmail.com)>
2022-12-01 23:18:04 +0100

    add dark-mode add menu transition with JS

--- js/script.js
+++ js/script.js
@@ -3,9 +3,65 @@
 const btn = document.querySelector('.btn');
 const menu = document.querySelector('.menu-contener');
 
+
+// menu burger translation avec toggle
+
+const barreMenu = document.querySelector(".barre-menu");
+const barres = document.querySelectorAll(".barre");
+// on donne des noms de variable aux neux pour mieux les utiliser
+const [a, b, c] = barres;
+// on set les valeurs pour avoir les bonnes conditions dans l'event, pour les rotation
+b.style.display = "block";
+
+a.style.transform = "rotate(0deg)";
+c.style.transform = "rotate(0deg)";
+
+// on set les valeurs pour avoir les bonnes conditions dans l'event, pour les écartement des barres
+
+const menuTitle = document.querySelectorAll(".menu-title");
+const [d, e, f] = menuTitle;
+d.style.padding = "0";
+e.style.padding = "0";
+f.style.padding = "0";
+
+// events de la selection du menu burger
+
 btn.addEventListener('click', () => {
+
     menu.classList.toggle("active");
+
+    a.classList.toggle("cross");
+    b.classList.toggle("cross");
+    c.classList.toggle("cross");
+// active la rotation si l'event est vrais le désactive dans le cas contaire 
+    if (a.style.transform === "rotate(0deg)") {
+        // a.style.transformOrigin= "center";
+        a.style.transform = "rotate(45deg)";
+        // c.style.transformOrigin= "center";
+        c.style.transform = "rotate(-45deg)";
+    } else {
+        a.style.transform = "rotate(0deg)";
+        c.style.transform = "rotate(0deg)";
+    }
+// met la barre du milieux en dehor du html pour le cacher
+    if (b.style.display === "block") {
+        b.style.display = "none";
+    } else {
+        b.style.display = "block";
+    }
+// annule le padding des barres pour mieux les placer
+    if (d.style.padding === "5") {
+        d.style.padding = "0";
+        e.style.padding = "0";
+        f.style.padding = "0";
+    } else {
+        d.style.padding = "5";
+        e.style.padding = "5";
+        f.style.padding = "5";
+    }
+
 });
+
 
 // Au survol du boutton contact affichez les contacts et les laisser visible tant que l'on survol est fait 
 
@@ -30,4 +86,30 @@ const video = document.querySelector('.video2');
 btnVideo.addEventListener('click', () => {
     video.classList.toggle("visible");
 });
+
+// change le style du background-color et la couleur du text pour appliquer le mode sombre 
+
+const darkMode = document.querySelector("input[name=dark-mode]");
+const label = document.querySelector("label");
+label.textContent = "🔅";
+
+const body = document.querySelector("body");
+
+darkMode.addEventListener("change", () => {
+    body.classList.toggle("dark-mode");
+    if (label.textContent === "🌙") {
+        label.textContent = "🔅";
+    } else {
+        label.textContent = "🌙";
+    }
+}
+);
+const styleInput = document.querySelector("#dark-mode");
+styleInput.style.width = "0px";
+
+
+// on change les immages des cards pour rendre la page plus dynamique
+
+
+
 
